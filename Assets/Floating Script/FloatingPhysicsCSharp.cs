2c512e26f86003c7da19d93557b32af27f16c5d5
ellieCalogero <elex95@gmail.com>
2015-03-22 18:58:19 +0100

    First Upload
    upload

---
+++ Assets/Floating Script/FloatingPhysicsCSharp.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class FloatingPhysicsCSharp : MonoBehaviour {
+    private float sine = 0.0f;
+    private int sw = 0; //switch needed during sine curve
+    private float timer = 0.0f;
+    private float xMovement;
+    
+    public float verticalSpeed = 1.0f;
+    public float verticalDistance = 1.0f;
+    public float horizontalSpeed = 1.0f;
+    
+
+
+	void Start () 
+	{		//Only executes when the program starts
+		xMovement = Random.Range (-.5f, .5f) * horizontalSpeed;   //random value between -0.5 and 0.5, causing some movement on the x axis.
+	}
+	void FixedUpdate () {   //FixedUpdate is used because it is used with physics
+
+	    if(sine < Mathf.PI && sw == 0)
+		{	//sine variable is fluctuating between 0 and Pi causing an up and down motion simulating floating, think sine curve
+	        sine += Time.deltaTime;
+	    }
+	    if(sine >= Mathf.PI){
+		    sw = 1;
+	   	}
+	    if(sine <= 0){
+		    sw = 0;
+		}
+	    if(sine >= 0 && sw == 1){
+	        sine = 0;
+	    }	
+		
+	    rigidbody.velocity = new Vector3(xMovement, Mathf.Sin(2*sine*verticalSpeed)*verticalDistance, 0);	//Adds the x axis movement and up and down motion to the object
+	
+	    if(timer < 10){ //increments timer
+		    timer += Time.deltaTime;
+	    }
+	}
+}
---
+++ Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMovement : MonoBehaviour {
+	
+	
+	public float lateralSpeed;
+	public float forwardSpeed;
+
+	public GameObject respawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		//movimento continuo della barca verso avanti
+		transform.Translate (Vector3.forward * forwardSpeed * Time.deltaTime);
+
+		//accelerometro con velocità
+//		Vector3 force = new Vector3 (Input.acceleration.x, 0, 0);
+//		transform.Translate (force*lateralSpeed*Time.deltaTime);
+
+
+		//movimento per pc
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector3.left*lateralSpeed*Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector3.right*lateralSpeed*Time.deltaTime);
+		}
+
+
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag == "obstacle")
+		{
+			Respawn();
+		}
+	}
+
+	void Respawn ()
+	{
+		transform.position = respawnPoint.transform.position;
+	}
+}
