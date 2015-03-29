381bc9757f9d8b933ede0e676611096cea307c30
ellieCalogero <elex95@gmail.com>
2015-03-29 18:16:53 +0200

    Main menu
    When we open the apk, there's a main menu with start and quit

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+
+
+	void Start () 
+	{
+	
+
+	}
+	
+
+	void Update () 
+	{
+
+	}
+
+	public void ChangeLevel() 
+	
+	{
+		Application.LoadLevel ("PrototipoBarchetta");
+	}
+
+	public void QuitApplication ()
+	
+	{
+		Application.Quit();
+	}
+}
