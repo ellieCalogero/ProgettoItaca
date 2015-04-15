662d0c9f113a738f615ffe5598026eee8619e3f0
ellieCalogero <elex95@gmail.com>
2015-04-15 18:31:33 +0200

    PauseMenu modified
    pause menu now is active with the click

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+
+	private Animator menuAnim;
+	bool menuOn = false;
+
+	//Button b= GetComponent<>;
+
+	void Awake () 
+	{
+		menuAnim = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+//		b.onClick.AddListener(()=>
+//		{
+//			BeginMenu ();
+//		});
+	}
+
+	public void BeginMenu ()
+	{
+		Debug.Log ("TASTO PREMUTO");
+		if (!menuOn)
+		{
+			menuAnim.SetTrigger ("FadeIn");
+			menuOn=true;
+			//Time.timeScale=0;
+		}
+
+
+		else
+		{
+			menuAnim.SetTrigger ("FadeOut");
+			menuOn=false;
+			//Time.timeScale=1;
+		}
+	}
+
+}
